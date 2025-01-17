6cb64afe9da9b20f079d95982dd85b5a40c1b48a
Librecz Bernadett <librecz.bernadett@magnetbank.hu>
2025-01-17 10:34:08 +0100

    Swagger UI

--- src/main/java/com/namyxc/locations/CreateLocationCommand.java
+++ src/main/java/com/namyxc/locations/CreateLocationCommand.java
@@ -1,5 +1,6 @@
 package com.namyxc.locations;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,13 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateLocationCommand {
+
+    @Schema(description = "name of location", example = "New York")
     private String name;
+
+    @Schema(description = "latitude of location", example = "40.730610")
     private double lat;
+
+    @Schema(description = "longitude of location", example = "-73.935242")
     private double lon;
 }
