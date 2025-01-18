a540c7019cdc1366ec0e6ef173c6f4e07ca2c8f5
Librecz Bernadett <librecz.bernadett@magnetbank.hu>
2025-01-18 09:54:06 +0100

    RestAssuredTest

---
+++ src/test/java/com/namyxc/locations/LocationsControllerRestAssuredIT.java
@@ -0,0 +1,48 @@
+package com.namyxc.locations;
+
+import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.with;
+import static io.restassured.module.mockmvc.RestAssuredMockMvc.given;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class LocationsControllerRestAssuredIT {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        RestAssuredMockMvc.mockMvc(mockMvc);
+        RestAssuredMockMvc.requestSpecification =
+                given()
+                        .contentType(ContentType.JSON)
+                        .accept(ContentType.JSON);
+    }
+
+    @Test
+    void testDeleteLocation() {
+        Integer id = with()
+                .body(new CreateLocationCommand("Delete", 1, 1))
+                .post("/locations")
+                .then()
+                .statusCode(201)
+                .body("name", equalTo("Delete"))
+                .extract().path("id");
+
+        with()
+                .delete("/locations/" + id)
+                .then()
+                .statusCode(204);
+    }
+}
