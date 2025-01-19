0a94551197170426ecb188ef0ebd23a4955fd4ff
Librecz Bernadett <librecz.bernadett@magnetbank.hu>
2025-01-19 11:44:20 +0100

    Integrációs tesztelés - MariaDB

--- src/test/java/com/namyxc/locations/LocationsControllerRestTemplateIT.java
+++ src/test/java/com/namyxc/locations/LocationsControllerRestTemplateIT.java
@@ -1,29 +1,27 @@
 package com.namyxc.locations;
 
 import com.namyxc.locations.dtos.LocationDto;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpMethod;
+import org.springframework.test.context.jdbc.Sql;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Sql(statements = "delete from locations")
 public class LocationsControllerRestTemplateIT {
 
     @Autowired
     TestRestTemplate template;
 
-    @Autowired
-    LocationsService locationsService;
-
-    @Test
-    void testListLoactions() {
-        locationsService.deleteAllLocation();
+    @RepeatedTest(2)
+    void testListLocations() {
 
         template.postForObject("/locations", new CreateLocationCommand("test1", 1,1), LocationDto.class);
         template.postForObject("/locations", new CreateLocationCommand("test2", 2,2), LocationDto.class);
